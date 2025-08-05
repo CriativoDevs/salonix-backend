3e4869dd6a986a92612f246481be273d0964b0e9
NemoIII <pablo.iii@outlook.com>
2025-08-05 12:03:42 +0100

    BE-01: Creating the project base

---
+++ salonix_backend/settings.py
@@ -0,0 +1,136 @@
+import os
+import configparser
+
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+print("BASE_DIR:", BASE_DIR)
+print("INI PATH:", os.path.join(BASE_DIR, "settings.ini"))
+
+# Define qual ambiente está sendo usado
+ENV = os.getenv("DJANGO_ENV", "dev")  # Pode ser: dev, uat, prod
+
+# Lê o arquivo settings.ini
+ini_path = BASE_DIR / "settings.ini"
+parser = configparser.ConfigParser(interpolation=None)
+parser.read(ini_path)
+
+# Segurança
+SECRET_KEY = parser[ENV]["SECRET_KEY"]
+print(f"SECRET_KEY: {SECRET_KEY}")
+DEBUG = parser.getboolean(ENV, "DEBUG")
+ALLOWED_HOSTS = [h.strip() for h in parser[ENV]["ALLOWED_HOSTS"].split(",")]
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "corsheaders",
+]
+
+MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+ROOT_URLCONF = "salonix_backend.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "salonix_backend.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": parser[ENV]["DATABASE_ENGINE"],
+        "NAME": parser[ENV]["DATABASE_NAME"],
+        "USER": parser[ENV].get("DATABASE_USER", ""),
+        "PASSWORD": parser[ENV].get("DATABASE_PASSWORD", ""),
+        "HOST": parser[ENV].get("DATABASE_HOST", ""),
+        "PORT": parser[ENV].get("DATABASE_PORT", ""),
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.2/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "Europe/Lisbon"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
